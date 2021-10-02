39d2d9749e61ed5065a7d083b0c64af2343f138b
radufx <raduftw01@gmail.com>
2021-10-02 18:26:40 +0200

    Modified components and navbar.

---
+++ src/Components/greenAreas.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function greenAreas(){
+    return(
+        <div className="mainBoard">
+            <h1>Green Areas</h1>
+        </div>
+    )
+}
+
+export default greenAreas;
