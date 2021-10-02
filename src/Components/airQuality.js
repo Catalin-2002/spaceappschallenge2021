0b3546a0603afb73a869ca28c3cce39e6d669629
radufx <raduftw01@gmail.com>
2021-10-02 15:31:50 +0200

    Edited airQuality

--- src/Components/airQuality.js
+++ src/Components/airQuality.js
@@ -3,7 +3,7 @@ import React from 'react';
 function airQuality(){
     return(
         <div className="mainBoard">
-            <p>Paragraf</p>
+            <p>Testing the merging options of the branch.</p>
         </div>
     )
 }
