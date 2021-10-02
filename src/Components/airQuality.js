71f1b7e366ad9415c3657f11b0daaef0d8d66d75
SebicuS <sebicus0720@gmail.com>
2021-10-02 16:46:14 +0300

    paragraf adaugat in airQuality

--- src/Components/airQuality.js
+++ src/Components/airQuality.js
@@ -3,7 +3,9 @@ import React from 'react';
 function airQuality(){
     return(
         <div className="mainBoard">
-            
+            <p>
+                Text de proba
+            </p>
         </div>
     )
 }
