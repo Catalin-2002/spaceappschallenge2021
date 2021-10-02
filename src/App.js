4433cd259fc74273dff7c039051b80bd49fd36db
radufx <raduftw01@gmail.com>
2021-10-02 15:19:18 +0200

    Added react-router-dom dependency.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import airQuality from './Components/airQuality';
+
 function App() {
   return (
-    <div> Test commit-push x2 </div>
+    <>
+    <Router>
+      <Switch>
+        <Route path='/airquality' component = {airQuality}/>
+      </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Components/airQuality.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function airQuality(){
+    return(
+        <div className="mainBoard">
+            
+        </div>
+    )
+}
+
+export default airQuality;
