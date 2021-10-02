19ce6dfd88f4802913a90645482183753373b1cb
radufx <raduftw01@gmail.com>
2021-10-02 17:31:26 +0200

    Addedd NavBar, Button class, routes.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import airQuality from './Components/airQuality';
+import Home from './Pages/Home';
+import Navbar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <>
     <Router>
+      <Navbar/>
       <Switch>
+        <Route path='/' exact component = {Home}/>
         <Route path='/airquality' component = {airQuality}/>
       </Switch>
       </Router>
---
+++ src/Components/Button/Buton.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Button.css';
+import { Link } from 'react-router-dom';
+
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
+
+const SIZES = ['btn--medium', 'btn--large'];
+
+const PAGES = ['Home', 'airQuality'];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+  links,
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+  const checkButtonLink = links
+
+  return (
+    <Link to={checkButtonLink} className='btn-mobile'>
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
